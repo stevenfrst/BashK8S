a2d94575bae8d1a2b9b2acc87a2a9ce6d9b28433
stevenfrst <oppaidaisuki363@gmail.com>
2019-03-16 08:02:16 +0700

    Setup Untuk Node Master

---
+++ k8setupmaster.sh
@@ -0,0 +1,4 @@
+sudo apt update; sudo apt upgrade -y; sudo apt autoremove -y
+sudo apt install -y docker.io; sudo docker version
+sudo apt install -y apt-transport-https; curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+vim kubernetes.list
