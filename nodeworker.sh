af97e8cf72fecead7a92fe78860bc86cf3c0ab25
Steven Humam <oppaidaisuki363@gmail.com>
2019-03-16 08:31:01 +0700

    Update nodeworker.sh

--- nodeworker.sh
+++ nodeworker.sh
@@ -1,7 +1,7 @@
 sudo apt update; sudo apt upgrade -y; sudo apt autoremove -y
 sudo apt install -y docker.io; sudo docker version
 sudo apt install -y apt-transport-https; curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-vim kubernetes.list
+nano kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 sudo mv kubernetes.list /etc/apt/sources.list.d/kubernetes.list
 sudo apt update; sudo apt install -y kubectl kubelet kubeadm
